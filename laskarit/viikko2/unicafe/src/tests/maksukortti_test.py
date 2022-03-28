77f8392322245601b63f83b74707aa93877fc953
Minna Ronka <minna.ronka@gmail.com>
2022-03-28 10:00:07 +0300

    Fix tests maksukortti

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -3,26 +3,26 @@ from maksukortti import Maksukortti
 
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
-        self.maksukortti = Maksukortti(10)
+        self.maksukortti = Maksukortti(1000)
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
 
     def test_kortin_saldo_alussa_oikein(self):
-        self.assertEqual(str(self.maksukortti), "saldo: 10")
+        self.assertEqual(str(self.maksukortti), "saldo: 10.0")
 
     def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
-        self.maksukortti.lataa_rahaa(10)
-        self.assertEqual(str(self.maksukortti), "saldo: 20")
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(str(self.maksukortti), "saldo: 20.0")
 
     def test_saldo_vahenee_oikein_kun_rahaa_on_tarpeeksi(self):
-        self.maksukortti.ota_rahaa(5)
-        self.assertEqual(str(self.maksukortti), "saldo: 5")
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "saldo: 5.0")
 
     def test_saldo_ei_muutu_jos_rahaa_ei_tarpeeksi(self):
-        self.maksukortti.ota_rahaa(15)
-        self.assertEqual(str(self.maksukortti), "saldo: 10")
+        self.maksukortti.ota_rahaa(1500)
+        self.assertEqual(str(self.maksukortti), "saldo: 10.0")
 
     def test_metodi_palauttaa_true_jos_rahat_riittivat_muuten_false(self):
-        self.assertTrue(self.maksukortti.ota_rahaa(5))
-        self.assertFalse(self.maksukortti.ota_rahaa(15))
+        self.assertTrue(self.maksukortti.ota_rahaa(500))
+        self.assertFalse(self.maksukortti.ota_rahaa(1500))
