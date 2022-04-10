8eeefa875258c39ce4a62b4c22c2a31d104a4ea1
Minna Ronka <minna.ronka@gmail.com>
2022-04-10 14:25:19 +0300

    Add tasks format (autopep8), lint (pylint)

--- tasks.py
+++ tasks.py
@@ -23,3 +23,18 @@ def coverage(ctx):
 @task(coverage)
 def coverage_report(ctx):
     ctx.run('coverage html', pty=True)
+
+
+'''formats code in src, use:
+    $ poetry run invoke format
+'''
+@task
+def format(ctx):
+    ctx.run('autopep8 --in-place --recursive src', pty=True)
+
+'''tests quality of code using pylint, use:
+    $ poetry run invoke lint
+'''
+@task
+def lint(ctx):
+    ctx.run('pylint src')
