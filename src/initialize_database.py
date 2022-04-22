a5549fc6de924c1a632a6f5ba636cef12c8d1020
Minna Ronka <minna.ronka@gmail.com>
2022-04-22 09:25:23 +0300

    Add table Persons and table Practices to schema

--- src/initialize_database.py
+++ src/initialize_database.py
@@ -8,8 +8,10 @@ from database_connection import get_database_connection
 
 def drop_tables(connection):
     cursor = connection.cursor()
-    cursor.execute("DROP TABLE IF EXISTS Words;")
+    cursor.execute("DROP TABLE IF EXISTS Practices;")
     cursor.execute("DROP TABLE IF EXISTS Translations;")
+    cursor.execute("DROP TABLE IF EXISTS Persons;")
+    cursor.execute("DROP TABLE IF EXISTS Words;")
 
     connection.commit()
 
@@ -25,8 +27,23 @@ def create_tables(connection):
 
     cursor.execute(''' CREATE TABLE Translations (
                     id INTEGER PRIMARY KEY,
-                    word_orig_id INTEGER,
-                    word_transl_id INTEGER
+                    word_orig_id INTEGER REFERENCE Words,
+                    word_transl_id INTEGER REFERENCE Words
+                    );
+                    ''')
+
+    cursor.execute(''' CREATE TABLE Persons (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT UNIQUE NOT NULL,
+                    password INTEGER
+                    );
+                    ''')
+
+    cursor.execute(''' CREATE TABLE Practices (
+                    id INTEGER PRIMARY KEY,
+                    person_id INTEGER REFERENCE Persons NOT NULL,
+                    translation_id INTEGER REFERENCE Translations NOT NULL,
+                    practicing_points_left INTEGER
                     );
                     ''')
     connection.commit()
